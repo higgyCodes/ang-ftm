6bea6a6080f36fc6437e33f0bb50926fc8f30aca
pathiggins13 <pat_higgins@hotmail.com>
2015-11-01 16:07:00 -0700

    changes some css

--- app/scripts/controllers/cmvctrl.js
+++ app/scripts/controllers/cmvctrl.js
@@ -1,10 +1,29 @@
-utahVotes.controller('cmvCtrl', ['$scope', function($scope){
+utahVotes.controller('cmvCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray){
+
+	var ref = new Firebase("https://utah-votes.firebaseio.com/cmventries");
 
 	$scope.entry = {
-		name: "Name",
-		status: "Candidate Running Status",
-		user: "Total"}
+		status: "Candidate Running Status"}
 
+	$scope.test = function() {
+		var entry = $scope.entry.note
+		$scope.entry.note = [{entry}];
+		var file = $scope.entry
+		var list = $firebaseArray(ref);
+		list.$add(file).then(function(ref) {
+		  var id = ref.key();
+		  console.log("added record with id " + id);
+		  list.$indexFor(id); // returns location in the array
+	});
+
+	$scope.entry = {
+		status: "Candidate Running Status"}
+
+	}
+
+	
+    var query = ref.limitToLast(10);
+	$scope.list = $firebaseArray(query);
 
 
 
